e7ded0515876ad76220f156747bad32029c35578
Ghareeb Falazi <ghareeb.falazi@hotmail.com>
2024-09-29 15:39:45 +0200

    initial commit

---
+++ Start.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+SLEEP_SECONDS=1
+FABRIC_CHANNEL_NAME="mychannel"
+FABRIC_CHAINCODE_NAME="fabric-rmsc"
+
+
+echo -e "###############################"
+echo -e "####    Deploying Fabric   ####"
+echo -e "###############################\n"
+sleep ${SLEEP_SECONDS}
+
+cd fabric
+
+if [ -d "fabric-samples" ]; then
+    echo "fabric-samples directory already exists..."
+else
+    echo "Directory fabric-samples does not exist. Running install-fabric.sh..."
+    ./install-fabric.sh
+fi
+
+echo -e "######################################"
+echo -e "####    Starting Fabric Network   ####"
+echo -e "######################################\n"
+sleep ${SLEEP_SECONDS}
+
+cd fabric-samples/test-network
+./network.sh down
+./network.sh up createChannel -c "$FABRIC_CHANNEL_NAME" -ca
+
+
+echo -e "###############################################"
+echo -e "####    Deploying Fabric Smart Contracts   ####"
+echo -e "###############################################\n"
+sleep ${SLEEP_SECONDS}
+
+cd ../..
+mkdir fabric-rmsc
+cd "$FABRIC_CHAINCODE_NAME"
+git clone https://github.com/TIHBS/fabric-resource-manager.git .
+
+cd ../fabric-samples/test-network
+./network.sh deployCC -ccn "$FABRIC_CHAINCODE_NAME" -ccp ../../$FABRIC_CHAINCODE_NAME -ccl javascript
