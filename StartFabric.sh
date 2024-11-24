b081d1744e70740f746a044ee8bc205fc1588dcc
Ghareeb Falazi <ghareeb.falazi@hotmail.com>
2024-11-24 22:49:14 +0100

    enahnce fabric startup

--- StartFabric.sh
+++ StartFabric.sh
@@ -33,10 +33,24 @@ echo -e "####    Deploying Fabric Smart Contracts   ####"
 echo -e "###############################################\n\n"
 sleep ${SLEEP_SECONDS}
 
-cd ../..
-mkdir fabric-rmsc
-cd "$FABRIC_CHAINCODE_NAME"
-git clone https://github.com/TIHBS/fabric-resource-manager.git .
+if [ -d "../../$FABRIC_CHAINCODE_NAME" ]; then
+    echo "$FABRIC_CHAINCODE_NAME already exists..."
+    cd "../../$FABRIC_CHAINCODE_NAME"
+else
+    echo "Directory $FABRIC_CHAINCODE_NAME does not exist. Cloning smart contracts..."
+    cd ../..
+    mkdir "$FABRIC_CHAINCODE_NAME"
+    cd "$FABRIC_CHAINCODE_NAME"
+    git clone https://github.com/TIHBS/fabric-resource-manager.git .
+fi
 
 cd ../fabric-samples/test-network
 ./network.sh deployCC -ccn "$FABRIC_CHAINCODE_NAME" -ccp ../../$FABRIC_CHAINCODE_NAME -ccl javascript
+
+echo -e "\n\n"
+echo -e "###############################################"
+echo -e "##    Initializing Fabric Smart Contracts   ###"
+echo -e "###############################################\n\n"
+sleep ${SLEEP_SECONDS}
+
+./network.sh cc invoke -c "$FABRIC_CHANNEL_NAME" -ccn "$FABRIC_CHAINCODE_NAME" -ccic '{"Args":["BasicFlightBookingManager:InitLedger"]}'
